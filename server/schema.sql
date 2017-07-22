654724b437fbfeed82ec3547dbd1e3a215db16e2
Jmetev1 <jmetevier@gmail.com>
2017-07-21 19:43:26 -0500

    first steps

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,25 @@
+drop database chat;
 CREATE DATABASE chat;
-
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE Users (
+  user_Id int,
+  userName text,
+  PRIMARY KEY (user_Id)  
 );
 
+CREATE TABLE Messages (
+  message_Id int,
+  roomName text,
+  message text,
+  sender_Id int,
+  PRIMARY KEY (message_Id),
+  FOREIGN KEY (sender_Id) REFERENCES Users(user_Id)
+);
+
+
+
+
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
